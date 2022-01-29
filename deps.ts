f48df8a87efca3ff45936104789d5ba859fb740d
authcompanion <paul@pmprosociety.com>
2022-01-28 22:22:58 -0500

    chore: deps bump & support deno 1.18.1

--- deps.ts
+++ deps.ts
@@ -3,9 +3,9 @@ export {
   isHttpError,
   Router,
   Status,
-} from "https://deno.land/x/oak@v10.1.0/mod.ts";
+} from "https://deno.land/x/oak@v10.1.1/mod.ts";
 export { DB } from "https://deno.land/x/sqlite@v3.2.0/mod.ts";
-export { compare, hash } from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
+export { compare, hash } from "https://deno.land/x/bcrypt@v0.3.0/mod.ts";
 export {
   create,
   decode,
@@ -14,9 +14,9 @@ export {
 } from "https://deno.land/x/djwt@v2.4/mod.ts";
 export type { Header, Payload } from "https://deno.land/x/djwt@v2.4/mod.ts";
 //remove me at some point
-export { v4 } from "https://deno.land/std@0.119.0/uuid/mod.ts";
-export * as log from "https://deno.land/std@0.119.0/log/mod.ts";
-export { LogRecord } from "https://deno.land/std@0.119.0/log/logger.ts";
+export { v4 } from "https://deno.land/std@0.123.0/uuid/mod.ts";
+export * as log from "https://deno.land/std@0.123.0/log/mod.ts";
+export { LogRecord } from "https://deno.land/std@0.123.0/log/logger.ts";
 export { SmtpClient } from "https://deno.land/x/smtp@v0.7.0/mod.ts";
 export type { ConnectConfigWithAuthentication } from "https://deno.land/x/smtp@v0.7.0/mod.ts";
 export {
@@ -24,9 +24,8 @@ export {
   green,
   red,
   yellow,
-} from "https://deno.land/std@0.119.0/fmt/colors.ts";
-export { format } from "https://deno.land/std@0.119.0/datetime/mod.ts";
+} from "https://deno.land/std@0.123.0/fmt/colors.ts";
+export { format } from "https://deno.land/std@0.123.0/datetime/mod.ts";
 import * as superstruct from "https://cdn.skypack.dev/superstruct";
 export { superstruct };
-export { delay } from "https://deno.land/x/delay@v0.2.0/mod.ts";
-export * as base64 from "https://deno.land/std@0.119.0/encoding/base64.ts";
+export * as base64 from "https://deno.land/std@0.123.0/encoding/base64.ts";
--- tests/serverIntegration.test.ts
+++ tests/serverIntegration.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.107.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.123.0/testing/asserts.ts";
 import { db } from "../db/db.ts";
 
 function purgeTestData() {
