8d3e0edb98d3ab40f2454a6e35526a2bdd98ba3e
authcompanion <paul@pmprosociety.com>
2022-02-26 21:44:11 -0500

    fix: integration tests no longer leak + deno fmt

--- db/db.ts
+++ db/db.ts
@@ -3,10 +3,10 @@ import { copy } from "https://deno.land/std@0.126.0/fs/mod.ts";
 import log from "../helpers/log.ts";
 
 try {
-    await copy("./db/users.db", "./db/prod/users.db"); // returns a promise
-    log.info("created a new users.db database");
+  await copy("./db/users.db", "./db/prod/users.db"); // returns a promise
+  log.info("created a new users.db database");
 } catch (error) {
-    log.info("existing users.db database is available");    
+  log.info("existing users.db database is available");
 }
 // Open sqlite users database
 const db = new DB("./db/prod/users.db", { mode: "write" });
