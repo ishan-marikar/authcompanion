25f82ace5257789fb53ab78f415770ad74d3037a
Teddy Schmitz <teddy@schmitz.hk>
2022-03-08 19:40:30 +0000

    cleanup telemetry

--- bin/server.ts
+++ bin/server.ts
@@ -1,27 +1,27 @@
 // @ts-nocheck
-import app from "../app.ts";
-import log from "../helpers/log.ts";
-import config from "../config.ts";
-import sendTelemetry from "../helpers/telemetry.ts";
+import app from '../app.ts';
+import log from '../helpers/log.ts';
+import config from '../config.ts';
+import { sendTelemetry } from '../helpers/telemetry.ts';
 
 const PORT = Number(config.AUTHPORT ?? 3002);
 const controller = new AbortController();
 const { signal } = controller;
 
-app.addEventListener("listen", ({ secure, hostname, port }) => {
-  const protocol = secure ? "https://" : "http://";
-  log.debug(`HTTPS is ${secure ? "on" : "off"}`);
-  const url = `${protocol}${hostname ?? "localhost"}:${port}`;
+app.addEventListener('listen', ({ secure, hostname, port }) => {
+  const protocol = secure ? 'https://' : 'http://';
+  log.debug(`HTTPS is ${secure ? 'on' : 'off'}`);
+  const url = `${protocol}${hostname ?? 'localhost'}:${port}`;
   console.log(`
   ################################################
   🖥️   UI Client on: http://localhost:${port}/client/v1/login
   🚀  API Server on: http://localhost:${port}/api/v1/auth/
   ################################################
   `);
-  console.log("Use CTRL-C to shutdown AuthCompanion");
+  console.log('Use CTRL-C to shutdown AuthCompanion');
 });
 
-sendTelemetry({ event_name: "open_server_event" });
+sendTelemetry({ event_name: 'open_server_event' });
 const serverlisten = app.listen({ port: PORT, signal });
 
 const sigIntHandler = () => {
@@ -31,8 +31,8 @@ const sigIntHandler = () => {
   Deno.exit();
 };
 
-Deno.addSignalListener("SIGINT", sigIntHandler);
+Deno.addSignalListener('SIGINT', sigIntHandler);
 
-Deno.addSignalListener("SIGTERM", sigIntHandler);
+Deno.addSignalListener('SIGTERM', sigIntHandler);
 
 await serverlisten;
--- helpers/telemetry.ts
+++ helpers/telemetry.ts
@@ -1,18 +1,16 @@
-// @ts-nocheck
-
-async function sendTelemetry(data = {}) {
+export async function sendTelemetry(data: {
+  event_name: string;
+}): Promise<Response> {
   const response = await fetch(
-    "https://event-server.fly.dev/api/v1/s2s/event",
+    'https://event-server.fly.dev/api/v1/s2s/event',
     {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        "X-Auth-Token": "s2s.rikq0t4hx3z3tai57xhnr.0ewnnsgza7793e9zws8ccpk",
+        'Content-Type': 'application/json',
+        'X-Auth-Token': 's2s.rikq0t4hx3z3tai57xhnr.0ewnnsgza7793e9zws8ccpk',
       },
       body: JSON.stringify(data),
-    },
+    }
   );
-  return await response.json();
+  return response;
 }
-
-export default sendTelemetry;
